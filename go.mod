48bfa7a03d6c58c48fb90d0cb5a81475f2cfbb55
CheneyDev <cheneydev@gmail.com>
2024-01-06 01:06:21 +0800

    try list files

--- go.mod
+++ go.mod
@@ -3,6 +3,28 @@ module twitter-to-telegram
 go 1.21.4
 
 require (
-	github.com/aws/aws-sdk-go v1.49.14 // indirect
+	github.com/aws/aws-sdk-go v1.49.15
+	github.com/aws/aws-sdk-go-v2 v1.24.1
+	github.com/aws/aws-sdk-go-v2/config v1.26.3
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.47.8
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.5.4 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.16.14 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.2.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.2.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.16.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.18.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.26.7 // indirect
+	github.com/aws/smithy-go v1.19.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
 	"log"
-	"net/http"
 	"os"
-	"twitter-to-telegram/api"
-	"twitter-to-telegram/utils"
+	"twitter-to-telegram/service/s3-storage"
 )
 
 type RequestBody struct {
@@ -17,49 +16,69 @@ type RequestBody struct {
 }
 
 func main() {
-	rapidApiKey := os.Getenv("RAPID_API_KEY")
-	rapidApiHost := os.Getenv("RAPID_API_HOST")
-	expectedAuthKey := os.Getenv("AUTH_KEY")
-
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		var reqBody RequestBody
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body", http.StatusInternalServerError)
-			return
-		}
-		defer r.Body.Close()
-
-		err = json.Unmarshal(body, &reqBody)
-		if err != nil {
-			http.Error(w, "Error parsing JSON request body", http.StatusBadRequest)
-			return
-		}
-
-		if reqBody.TweetURL == "" {
-			http.Error(w, "tweetUrl is required", http.StatusBadRequest)
-			return
-		}
-
-		if expectedAuthKey != reqBody.AuthKey {
-			http.Error(w, "Invalid authentication key", http.StatusUnauthorized)
-			return
-		}
-
-		res, err := api.GetTweet(reqBody.TweetURL, rapidApiKey, rapidApiHost)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error getting tweet: %v", err), http.StatusInternalServerError)
-			return
-		}
-		tweet, err := utils.HandleTweet(res)
-		tweetText := fmt.Sprintf("%s\n%s", tweet.Text, tweet.Author.ScreenName)
-		tweetPhotos := tweet.Media.Photo
-		tweetVideos := tweet.Media.Video
-		// 打印结果
-		fmt.Fprintf(w, "Response: %v\n%v\n%v\n", tweetText, tweetPhotos, tweetVideos)
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
 	}
-	http.HandleFunc("/getTweet", handler)
+	bucketName := os.Getenv("S3_BUCKET_NAME")
+	s3Client := s3_storage.NewS3Client()
+	input := &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	}
+	result, err := s3Client.Service.ListObjectsV2(input)
+	if err != nil {
+		log.Fatalf("无法列出桶中的对象: %s", err)
+	}
+	//print
+	fmt.Println(result, bucketName)
 
-	fmt.Println("Server is starting...")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
+
+//func main() {
+//	rapidApiKey := os.Getenv("RAPID_API_KEY")
+//	rapidApiHost := os.Getenv("RAPID_API_HOST")
+//	expectedAuthKey := os.Getenv("AUTH_KEY")
+//
+//	handler := func(w http.ResponseWriter, r *http.Request) {
+//		var reqBody RequestBody
+//		body, err := ioutil.ReadAll(r.Body)
+//		if err != nil {
+//			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+//			return
+//		}
+//		defer r.Body.Close()
+//
+//		err = json.Unmarshal(body, &reqBody)
+//		if err != nil {
+//			http.Error(w, "Error parsing JSON request body", http.StatusBadRequest)
+//			return
+//		}
+//
+//		if reqBody.TweetURL == "" {
+//			http.Error(w, "tweetUrl is required", http.StatusBadRequest)
+//			return
+//		}
+//
+//		if expectedAuthKey != reqBody.AuthKey {
+//			http.Error(w, "Invalid authentication key", http.StatusUnauthorized)
+//			return
+//		}
+//
+//		res, err := api.GetTweet(reqBody.TweetURL, rapidApiKey, rapidApiHost)
+//		if err != nil {
+//			http.Error(w, fmt.Sprintf("Error getting tweet: %v", err), http.StatusInternalServerError)
+//			return
+//		}
+//		tweet, err := utils.HandleTweet(res)
+//		tweetText := fmt.Sprintf("%s\n%s", tweet.Text, tweet.Author.ScreenName)
+//		tweetPhotos := tweet.Media.Photo
+//		tweetVideos := tweet.Media.Video
+//		// 打印结果
+//		fmt.Fprintf(w, "Response: %v\n%v\n%v\n", tweetText, tweetPhotos, tweetVideos)
+//	}
+//	http.HandleFunc("/getTweet", handler)
+//
+//	fmt.Println("Server is starting...")
+//	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+//}
