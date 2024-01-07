6e6e9a2c910f339d527c23277517bb34eb61c7b2
CheneyDev <cheneydev@gmail.com>
2024-01-07 22:09:27 +0800

    update README and try to uploading file in code

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # Tweet Keeper
 
-一个把 Tweet 转发到 Telegram 的工具
+一个把 Tweet 转存到 Notion 的工具
 
 ### To Do
 
-Docker 防火墙问题
+* Docker 防火墙
+* 获取用户点赞的推文（所有、前一天），触发方式：快捷指令
+* 手机端分享转存到 Notion，触发方式：快捷指令
+* 获取用户所有媒体推文 （所有、前一天），触发方式：快捷指令
+
+### S3 文件结构
+
+bucket-name/user-tweets/username/YYYY/MM/DD/username-tweet-YYYY-MM-DD-XXXX.ext
+bucket-name/user-likes/username/YYYY/MM/DD/username-like-YYYY-MM-DD-XXXX.ext
+
+### Tweet 需要提取的数据
+
+用户名，用户昵称，发布日期，推文，媒体文件
 
 文档
 
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/joho/godotenv"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
-	"tweet-keeper/service/s3-storage"
+	"tweet-keeper/api"
+	"tweet-keeper/utils"
 )
 
 type RequestBody struct {
@@ -15,70 +16,95 @@ type RequestBody struct {
 	AuthKey  string `json:"authKey"`
 }
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env file")
-		return
-	}
-	bucketName := os.Getenv("S3_BUCKET_NAME")
-	s3Client := s3_storage.NewS3Client()
-	input := &s3.ListObjectsV2Input{
-		Bucket: aws.String(bucketName),
-	}
-	result, err := s3Client.Service.ListObjectsV2(input)
-	if err != nil {
-		log.Fatalf("无法列出桶中的对象: %s", err)
-	}
-	//print
-	fmt.Println(result, bucketName)
-
-}
-
 //func main() {
-//	rapidApiKey := os.Getenv("RAPID_API_KEY")
-//	rapidApiHost := os.Getenv("RAPID_API_HOST")
-//	expectedAuthKey := os.Getenv("AUTH_KEY")
-//
-//	handler := func(w http.ResponseWriter, r *http.Request) {
-//		var reqBody RequestBody
-//		body, err := ioutil.ReadAll(r.Body)
-//		if err != nil {
-//			http.Error(w, "Error reading request body", http.StatusInternalServerError)
-//			return
-//		}
-//		defer r.Body.Close()
-//
-//		err = json.Unmarshal(body, &reqBody)
-//		if err != nil {
-//			http.Error(w, "Error parsing JSON request body", http.StatusBadRequest)
-//			return
-//		}
-//
-//		if reqBody.TweetURL == "" {
-//			http.Error(w, "tweetUrl is required", http.StatusBadRequest)
-//			return
-//		}
-//
-//		if expectedAuthKey != reqBody.AuthKey {
-//			http.Error(w, "Invalid authentication key", http.StatusUnauthorized)
-//			return
-//		}
-//
-//		res, err := api.GetTweet(reqBody.TweetURL, rapidApiKey, rapidApiHost)
-//		if err != nil {
-//			http.Error(w, fmt.Sprintf("Error getting tweet: %v", err), http.StatusInternalServerError)
-//			return
-//		}
-//		tweet, err := utils.HandleTweet(res)
-//		tweetText := fmt.Sprintf("%s\n%s", tweet.Text, tweet.Author.ScreenName)
-//		tweetPhotos := tweet.Media.Photo
-//		tweetVideos := tweet.Media.Video
-//		// 打印结果
-//		fmt.Fprintf(w, "Response: %v\n%v\n%v\n", tweetText, tweetPhotos, tweetVideos)
+//	// 加载环境变量，如果不存在则使用默认值
+//	if err := godotenv.Load(); err != nil {
+//		log.Println("Warning: Error loading .env file, using default values")
 //	}
-//	http.HandleFunc("/getTweet", handler)
 //
-//	fmt.Println("Server is starting...")
-//	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+//	// 获取S3桶名，如果环境变量不存在则使用默认桶名
+//	bucketName := os.Getenv("S3_BUCKET_NAME")
+//	if bucketName == "" {
+//		log.Fatal("S3_BUCKET_NAME environment variable is not set.")
+//	}
+//
+//	// 创建S3客户端实例
+//	s3Client := s3_storage.NewS3Client()
+//
+//	// 打开文件
+//	file, err := os.Open("test.txt")
+//	if err != nil {
+//		log.Fatalf("Error opening file: %s", err)
+//	}
+//	defer file.Close() // 确保文件最终会被关闭
+//
+//	// 上传文件到S3
+//	_, err = s3Client.Service.PutObject(&s3.PutObjectInput{
+//		Bucket: aws.String(bucketName),
+//		Key:    aws.String("./tt/testhhh.txt"),
+//		Body:   file,
+//	})
+//	if err != nil {
+//		log.Fatalf("Failed to upload object to S3: %s", err)
+//	}
+//
+//	// 列出S3桶中的对象
+//	input := &s3.ListObjectsV2Input{
+//		Bucket: aws.String(bucketName),
+//	}
+//	result, err := s3Client.Service.ListObjectsV2(input)
+//	if err != nil {
+//		log.Fatalf("Unable to list objects in bucket: %s", err)
+//	}
+//
+//	// 打印结果和桶名
+//	fmt.Println("List of objects in bucket:", result)
 //}
+
+func main() {
+	rapidApiKey := os.Getenv("RAPID_API_KEY")
+	rapidApiHost := os.Getenv("RAPID_API_HOST")
+	expectedAuthKey := os.Getenv("AUTH_KEY")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		var reqBody RequestBody
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		err = json.Unmarshal(body, &reqBody)
+		if err != nil {
+			http.Error(w, "Error parsing JSON request body", http.StatusBadRequest)
+			return
+		}
+
+		if reqBody.TweetURL == "" {
+			http.Error(w, "tweetUrl is required", http.StatusBadRequest)
+			return
+		}
+
+		if expectedAuthKey != reqBody.AuthKey {
+			http.Error(w, "Invalid authentication key", http.StatusUnauthorized)
+			return
+		}
+
+		res, err := api.GetTweet(reqBody.TweetURL, rapidApiKey, rapidApiHost)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error getting tweet: %v", err), http.StatusInternalServerError)
+			return
+		}
+		tweet, err := utils.HandleTweet(res)
+		tweetText := fmt.Sprintf("%s\n%s", tweet.Text, tweet.Author.ScreenName)
+		tweetPhotos := tweet.Media.Photo
+		tweetVideos := tweet.Media.Video
+		// 打印结果
+		fmt.Fprintf(w, "Response: %v\n%v\n%v\n", tweetText, tweetPhotos, tweetVideos)
+	}
+	http.HandleFunc("/getTweet", handler)
+
+	fmt.Println("Server is starting...")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
