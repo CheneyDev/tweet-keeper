5165c17811269340cef203457b46eebab4882f89
CheneyDev <cheneydev@gmail.com>
2024-01-21 22:16:24 +0800

    get max bitrate video url

--- main.go
+++ main.go
@@ -252,10 +252,6 @@ func main() {
 		if len(tweet.Media.Photo) > 0 {
 			tweetPhotos = tweet.Media.Photo
 			//fmt.Println(tweetPhotos[0].MediaURLHttps)
-			tweetPhotos = nil
-			if tweetPhotos == nil {
-				fmt.Println("nil")
-			}
 		} else {
 			tweetPhotos = nil
 		}
@@ -263,10 +259,18 @@ func main() {
 		var tweetVideos []utils.Video
 		if len(tweet.Media.Video) > 0 {
 			tweetVideos = tweet.Media.Video
+			maxBitrate := tweetVideos[0].Variants[0].Bitrate
+			maxVideoUrl := tweetVideos[0].Variants[0].URL
+			for _, variants := range tweetVideos[0].Variants {
+				if variants.Bitrate > maxBitrate {
+					maxBitrate = variants.Bitrate
+					maxVideoUrl = variants.URL
+				}
+			}
+			fmt.Println(maxBitrate, maxVideoUrl)
 		} else {
 			tweetVideos = nil
 		}
-		// 打印结果
 		_, err = fmt.Fprintf(w, "Response: %v\n%v\n%v\n", tweetText, tweetPhotos, tweetVideos)
 		if err != nil {
 			return
